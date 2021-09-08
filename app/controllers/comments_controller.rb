6508ef7c1491905ec315804eb2b2e32470cc8ba9
DU23G <urano.d23@gmail.com>
2021-09-08 19:15:59 +0900

    微修正（DM機能の追加前）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,4 +19,5 @@ class CommentsController < ApplicationController
       def comment_params
         params.require(:comment).permit(:content)
       end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,31 +8,23 @@ Rails.application.routes.draw do
   get "toppages/index" => "toppages#index"
   root "toppages#index"
 
-  # get 'tweets' => 'tweets#index'
-  # post 'tweets' => 'tweets#create'
-
-  # get 'tweets/:id' => 'tweets#show',as: 'tweet'
-  # patch 'tweets/:id' => 'tweets#update'
-  # delete 'tweets/:id' => 'tweets#destroy'
-  # get 'tweets/:id/edit' => 'tweets#edit', as:'edit_tweet'
-
   # 以下、書かれていなかったルーティング
   # get    'tweets/new'      => 'tweets#new'
   # put    'tweets/:id'      => 'tweets#update'
-   post "tweets/:id" => 'tweets#show'
+  
+  post "tweets/:id" => 'tweets#show'
 
   resources :tweets do
     member do
-      post 'take'
-      post 'cancel'
-      # get 'take'
-      # get 'cancel'
+      post 'take'       # get 'take'
+      post 'cancel'       # get 'cancel'
     end
     resources :likes, only: [:create, :destroy,]
     resources :remarks, only: [:create]
   end
   
-  resources :groups, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :comments, only: [:create]
+  resources :groups, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: [:create]
+  end
 
 end
