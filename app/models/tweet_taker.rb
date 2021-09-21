10c0204794fa131e08c9e9c9013ff9b39fdae057
DU23G <urano.d23@gmail.com>
2021-09-21 12:52:53 +0900

    Demoday用デザイン完了

--- app/models/tweet_taker.rb
+++ app/models/tweet_taker.rb
@@ -1,5 +1,6 @@
 class TweetTaker < ApplicationRecord    
     belongs_to :taker, class_name: 'User', foreign_key: 'taker_id'
     # has_many :tweets, dependent: :destroys
+    # belongs_to :tweet
     validates_uniqueness_of :tweet_id, scope: :taker_id
 end
