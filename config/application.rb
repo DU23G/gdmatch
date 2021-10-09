852f67ecaabfcbefaa9bb80063181c506d497ea3
DU23G <urano.d23@gmail.com>
2021-10-09 23:20:22 +0900

    再デプロイ修正14時刻問題解決3

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Gdmatch
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.time_zone = 'Asia/Tokyo'
+    config.time_zone = 'Asia/Tokyo'
 
     # config.eager_load_paths << Rails.root.join("extras")
   end
